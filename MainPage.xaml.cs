99ad5fb2f7d11bafb4a211fa779e082f60b92987
yaron <yaron@YARONIX>
2023-12-29 18:14:16 +0200

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace PhotoGallery
+{
+    public partial class MainPage : ContentPage
+    {
+        int current = 1;
+        List<string> imgs = new List<string>(3) { "piano1.jpg", "pianoo2.png", "piano3.png" };
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void UpPic(object sender, EventArgs e)
+        {
+            current++;
+            if (current >= imgs.Count-1)
+            {
+                UPbtn.IsEnabled = false;
+            }
+            if (current < imgs.Count)
+            {
+                pImg.Source = imgs[current];
+                DOWNbtn.IsEnabled = true;
+            }
+        }
+        private void DownPic(object sender, EventArgs e)
+        {
+            current--;
+            if (current <= 0)
+            {
+                DOWNbtn.IsEnabled = false;
+            }
+            if (current >= 0)
+            {
+                pImg.Source = imgs[current];
+                UPbtn.IsEnabled = true;
+            }
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PhotoGallery
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
