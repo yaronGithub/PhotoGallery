1ee4e90cd766392667d2d501d0f9ca8055defc57
yaronGithub <yaron.traitel@gmail.com>
2024-01-01 14:42:20 +0200

    great

---
+++ Monkey.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoGallery
+{
+    public class Monkey
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Details { get; set; }
+        public string ImageUrl { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name:{Name}, Location: {Location}";
+        }
+    }
+}
